25d5ec1b86720973862dd8da5dcbe0555c3f9fab
Lukas Steiger <lukas.steiger@outlook.com>
2016-10-04 22:04:28 +0200

    implement expres 'hello world' to prove setup is working

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-var http = require("http");
+var express = require("express");
+var app = express();
+
+const port = 3000;
+const hostname = '127.0.0.1';
+
+app.listen(port, hostname, () => {
+    console.log(`Server running @ http://${hostname}:${port}`);
+});
