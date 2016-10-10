8258dde14c75af89e62e26b0b37ae340668e8475
jengeler <jengeler@hsr.ch>
2016-10-10 12:58:25 +0200

    Added nedb; Implemented Methods for DB-Access

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,34 +1,67 @@
-var express = require('express')
+var express = require('express');
 var router = express.Router();
 
 // list all notes
 router.get('/', function(req, res) {
     console.log("/notes");
-    res.render("notes");
+    res.format({
+        'text/html': function(){
+            res.render("notes");
+        },
+        'application/json': function(){
+            res.send({});
+        }
+    });
+    /*store.all(function(err, note) {
+     console.log("GET /");
+     res.end();
+     });*/
 });
 
 // new note
 router.get('/add', function(req, res) {
     console.log("GET /notes/new");
-    res.render("add");
+    res.format({
+        'text/html': function(){
+            res.render("add");
+        },
+        'application/json': function(){
+            res.send();
+        }
+    });
 });
 
 // create note
 router.post('/', function(req, res) {
     console.log("POST /notes");
-    res.end();
+    store.add(req.body.title, req.body.description, req.body.importance,req.body.finishedUnitl, "unkown", function(err, note) {
+        console.log("POST /notes");
+        res.end();
+    });
 });
 
 // edit note
 router.get('/:id', function(req, res) {
     console.log("GET /notes/:id");
-    res.render("edit");
+
+    store.get(req.params.id, function(err, note) {
+        res.format({
+            'text/html': function(){
+                res.render("edit");
+            },
+            'application/json': function(){
+                res.json(note);
+            }
+        });
+    });
 });
 
 // write edited note
 router.put('/:id', function(req, res) {
     console.log("PUT /notes/:id");
-    res.end();
+    store.modify(req.params.id, req.body.title, req.body.description, req.body.importance,req.body.state,req.body.finishedUnitl, function(err, note) {
+        res.end();
+    });
 });
 
-module.exports = router
+module.exports = router;
