f44ac837769b49af5d289725bd28925fc0b2dd1d
Lukas Steiger <lukas.steiger@outlook.com>
2016-10-31 13:56:20 +0100

    add modules for session handling

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var session = require('express-session');
+var NedbStore = require('nedb-session-store')(session);
 var hbs = require('express-hbs');
 var hbsHelpers = require('./views/helpers/helpers');
 
@@ -15,6 +17,22 @@ app.set('views', __dirname + '/views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(
+    session({
+        secret: 'tomorrowlandi',
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            path: '/',
+            httpOnly: true,
+            maxAge: 365 * 24 * 60 * 60 * 1000   // 1 year
+        },
+        store: new NedbStore({
+            filename: 'data/session.db'
+        })
+    })
+);
 
 app.use(express.static(__dirname + '/public'));
 app.use(require('./controllers/routes'));
