7e2e611135f2213437f0bf18b07b65f326e9fa81
jengeler <jengeler@hsr.ch>
2016-10-31 11:16:18 +0100

    transfered stye parameter to hbs file. changed style for "show finished" button when selected;

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -3,6 +3,7 @@ var store = require('../service/noteStore');
 function publicGetAll(req, res, config){
     var sorting;
     var show;
+    var style;
     config.forEach(function(conf, index) {
         if(conf.type == 'sorting'){
             sorting = conf;
@@ -11,11 +12,15 @@ function publicGetAll(req, res, config){
         if(conf.type == 'show'){
             show = conf.value;
         }
+
+        if(conf.type == 'style'){
+            style = conf.value;
+        }
     });
     store.all(sorting.value, sorting.params.sortOrder, show, function(err, data, sortBy) {
         res.format({
             'text/html': function(){
-                res.render("notes", {notes: data, sortBy:sortBy, showFinished:show, sortOrder: sorting.params.sortOrder});
+                res.render("notes", {notes: data, sortBy:sortBy, showFinished:show, sortOrder: sorting.params.sortOrder, style:style.value});
             },
             'application/json': function(){
                 res.send({});
